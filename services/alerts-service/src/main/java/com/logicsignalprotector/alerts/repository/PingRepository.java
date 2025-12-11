2f4b58b6ef185f4cdc7aad6ce36ac8101047c63e
Nikitosik <filonenko77500@mail.ru>
2025-12-11 19:15:05 +0400

    Удален PingRepository и добавлены классы AlertsServiceApplication и PingController с базовой реализацией

--- services/alerts-service/src/main/java/com/logicsignalprotector/alerts/repository/PingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.logicsignalprotector.alerts.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PingRepository extends JpaRepository<PingEntity, Long> {
-  // Базовых методов JpaRepository достаточно: save, findAll, count, findById и т.д.
-}
